c679752b700aeff151f2e4c427b457027e480ef8
Pranav Maneriker <pranavmane@gmail.com>
2014-09-09 22:01:26 +0530

    Initial loading

--- game/level.cpp
+++ game/level.cpp
@@ -1,12 +1,26 @@
 class Level{
 	private:
-	int cur_level;
+	string cur_level_path;
+	string cur_level;
+	Model_OBJ *room;
+	Model_OBJ *player;
 	
 	public:
 	Level(int l);	
 }
 
-Level::Level(int l)
+Level::Level(string &l)
 {
-	cur_level = l
+	cur_level = l;
+	cur_level_path = "../rooms/" + l +".obj"
+	player = new Model_OBJ;
+	room = new Model_OBJ;
+	player.load("../models/Tux.obj")
+	room.load(cur_level_path);	//&cur_level_path[0]  might avoid warning but is it safe?	
+
+}
+
+Level::display()
+{
+	
 }
--- game/main.cpp
+++ game/main.cpp
@@ -15,26 +15,97 @@ class Game{
 	int score;
 	int health;
 	void initGL();
-	void start_level();
+	void initWindow();
 	Level level;
+	string level_name;
 	glWindow window;
+
 	
 	public:
 	Game();
 	void start();
-	
+	void keyPress(unsigned char key, int x, int y);
+	void specialKeyPress(int key, int x, int y);
+
 }
 
 Game::Game()
 {
-	Level level = new Level(1);
+	level_name = "Level_1";
+	Level level = new Level(level_name);
+}
+
+
+Game::keyPress(unsigned char key, int x, int y)
+{
+	Level->keyPress(key,x,y);
+}
+
+Game::specialKeyPress(unsigned char key, int x, int y)
+{
+	if(key == KEY_ESCAPE)
+	{
+		exit(0);	//add more graceful exit
+	}
+	Level->specialKeyPress(key,x,y);
+}
+
+Game::initGL()
+{
+	glMatrixMode(GL_PROJECTION);
+	glViewport(0, 0, win.width, win.height);
+	GLfloat aspect = (GLfloat) win.width / win.height;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(win.field_of_view_angle, aspect, win.z_near, win.z_far);
+	glMatrixMode(GL_MODELVIEW);
+	glShadeModel( GL_SMOOTH );
+	glClearColor( 0.0f, 0.1f, 0.0f, 0.5f );
+	glClearDepth( 1.0f );
+	glEnable( GL_DEPTH_TEST );
+	glDepthFunc( GL_LEQUAL );
+	glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
+
+	GLfloat amb_light[] = { 0.1, 0.1, 0.1, 1.0 };
+	GLfloat diffuse[] = { 0.6, 0.6, 0.6, 1 };
+	GLfloat specular[] = { 0.7, 0.7, 0.3, 1 };
+	glLightModelfv( GL_LIGHT_MODEL_AMBIENT, amb_light );
+	glLightfv( GL_LIGHT0, GL_DIFFUSE, diffuse );
+	glLightfv( GL_LIGHT0, GL_SPECULAR, specular );
+	glEnable( GL_LIGHT0 );
+	glEnable( GL_COLOR_MATERIAL );
+	glShadeModel( GL_SMOOTH );
+	glLightModeli( GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE );
+	glDepthFunc( GL_LEQUAL );
+	glEnable( GL_DEPTH_TEST );
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0); 
+}
+
+Game::initWindow()
+{
+	win.width = 640;
+	win.height = 480;
+	win.title = "Pogo flip";
+	win.field_of_view_angle = 45;
+	win.z_near = 1.0f;
+	win.z_far = 10.0f;
+	// initialize and run program
+	glutInit(&argc, argv);                                      // GLUT initialization
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );  // Display Mode
+	glutInitWindowSize(win.width,win.height);					// set window size
+	glutCreateWindow(win.title);								// create Window
+	glutDisplayFunc(level->display);									// register Display Function
+    	glutKeyboardFunc(keyPress);								// register Keyboard Handler
+    	glutSpecialFunc(specialKeyPress);								// register Keyboard Handler
+	initGL();
 }
 
 Game::start()
 {
-	initGl();
-	start_level(level);
+	initWindow();
 }
+
 
 
 
