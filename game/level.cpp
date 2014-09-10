4c0460e6f2707ea7b511b3b713cc598ecdc18464
Pranav Maneriker <pranavmane@gmail.com>
2014-09-10 15:19:55 +0530

    Opp level test

--- game/level.cpp
+++ game/level.cpp
@@ -11,28 +11,37 @@ class Level{
 	string cur_level_path;
 	string cur_level;
 	Model_OBJ *room;
+	Model_OBJ *inv;
 	Model_OBJ *player;
 	int g_rotation;
 	Player *p;
+	bool flipped;
 
 	public:
 	Level(string &l);	
 	void display();
 	void keyPress(unsigned char ch, int x, int y);
 	void specialKeyPress(int key, int x, int y);
+	void flip();
 };
+void Level::flip()
+{
+	flipped = !flipped;
+}
 
 Level::Level(string &l)
 {
 	cur_level = l;
-	cur_level_path = "../rooms/" + l +".obj";
+	cur_level_path = "../rooms/" + l +"_alt.obj";
+	inv = new Model_OBJ;
 	player = new Model_OBJ;
 	room = new Model_OBJ;
+	inv->Load("../rooms/Level_1.obj");
 	player->Load("../models/Tux.obj");
 	room->Load(&cur_level_path[0]);	//&cur_level_path[0]  might avoid warning but is it safe?	
 	g_rotation = 0;
 	p = new Player;
-	p->x = 0; p->y = -1; p->z = -4;	
+	p->x = 0; p->y = -1; p->z = -3;	
 	p->lookat_x = 0; p->lookat_y = 1; p->lookat_z = 0;
 }
 
@@ -43,11 +52,14 @@ void Level::display()
 	glRotatef(roty, 0, 1, 0);
 	glRotatef(rotx, 1, 0, 0);
 	glTranslatef(p->x,p->y,p->z);
+	player->Draw();	
+	if(flipped){
+		glRotatef(180,1,0,0);
+	}
 	//gluLookAt( p->x,p->y,p->z - 2, p->lookat_x + p->x,p->lookat_y,p->lookat_z +p->z, 0,1,0);
 	room->Draw();
-	glPushMatrix();
-	player->Draw();	
-	glPopMatrix();
+	glRotatef(180,1,0,0);
+	inv->Draw();
 	glutSwapBuffers();	
 	glFlush(); 
 }
@@ -71,6 +83,10 @@ void Level::keyPress(unsigned char key, int x, int y)
 	else if(key == 'd')
 	{
 		p->x-=0.3;
+	}
+	else if(key == 'q')
+	{
+		flip();
 	}
 	glutPostRedisplay();
 }
