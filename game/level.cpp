72c50512d7409057a347e2bce933de207f5e5c91
Arpit Shrivastava <arp.shri@gmail.com>
2014-10-06 10:46:39 +0530

    HUD and minor changes

--- game/level.cpp
+++ game/level.cpp
@@ -1,11 +1,24 @@
+#define PI 3.14159265359
 float rotx, roty, rotz;
 
 class Player{
 	public:
 	float x,y,z;
+	int points;
+	float curx, cury, curz;
 	float lookat_x,lookat_y,lookat_z;
 };
-
+class Target{
+	public:
+	float x,y,z;
+	int points;
+	bool reached;
+	Target (float a, float b, float c, int p)
+	{
+		x=a,y=b,z=c,points=p;
+		reached=false;
+	}
+};
 float flip_angle;	
 
 class Model_OBJ{
@@ -112,6 +125,7 @@ class Level{
 	float random_angle;
 
 	public:
+	vector<Target> targets;
 	Level(string &l);
 	void display();
 	void keyPress(unsigned char ch, int x, int y);
@@ -135,9 +149,15 @@ Level::Level(string &l)
 	room->Load(&cur_level_path[0]);	//&cur_level_path[0]  might avoid warning but is it safe?	
 	g_rotation = 0;
 	p = new Player;
-	p->x = 0; p->y = -1; p->z = -3;	
+	p->x = 0; p->y = -1; p->z = -3.4; p->points =0;
+	roty=54;
+	p->curx=p->cury=0; p->curz=3.4;	
 	p->lookat_x = 0; p->lookat_y = 1; p->lookat_z = 0;
 	random_angle = 0;
+	targets.push_back(Target(5, 0.2 , 2, 20));
+	targets.push_back(Target(5, 0.2 , 3, 20));
+	targets.push_back(Target(5, 0.2 , 1, 20));
+	
 }
 void Level::rotateFace()
 {
@@ -147,37 +167,100 @@ void Level::rotateFace()
 
 void Level::display()
 {
-	glClearColor(0,0,0,0);
+	glClearColor(0.7215,0.8627,0.9490,1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0);
 	glLoadIdentity();
 	glTranslatef(p->x,p->y,p->z);
 	glRotatef(roty, 0, 1, 0);
 	glRotatef(rotx, 1, 0, 0);
-	//glColor3f(0,0.5,0.5);
+
 
 	glPushMatrix();
-	glTranslatef(0,0.02,0);
-	player->DrawColor();	
+		glTranslatef(0,0.02,0);
+		//player->DrawColor();	
 	glPopMatrix();
-
 	glRotatef(flip_angle,1,0,0);
 	
-	//gluLookAt( p->x,p->y,p->z - 2, p->lookat_x + p->x,p->lookat_y,p->lookat_z +p->z, 0,1,0);
-	//glColor3f(0,1,0);
-	//room->Draw();
+	room->DrawColor();
+	glPushMatrix();
+		glRotatef(-roty+180, 0, 1, 0);
+		glRotatef(-rotx, 1, 0, 0);
+		glTranslatef(p->curx,p->cury,p->curz);
+		player->DrawColor();
+	glPopMatrix();
 	glPushMatrix();
 		glTranslatef(10,2,1);
 		glRotatef(random_angle, 0,1,0);
 		glScalef(0.5,0.5,0.5);
-		glColor3f(1,0.1,0);
 		randomFace->Draw();
 	glPopMatrix();
+	for(int i = 0; i < targets.size() ; i++)
+	{
+		if(targets[i].reached==false)
+		{
+			glPushMatrix();
+				glTranslatef(targets[i].x,targets[i].y,targets[i].z);
+				glRotatef(random_angle, 0,1,0);
+				glScalef(0.25,0.25,0.25);
+				randomFace->Draw();
+			glPopMatrix();
+		}
+	}
 	glTranslatef(0, -0.02,0);
 	glRotatef(180,1,0,0);
-	//glColor3f(1,1,1);
-	//inv->Draw();
+
+
+	inv->Draw();
+	
+	
+	
+	//HUD (Hud dabangg dabangg)
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	glOrtho(0.0, win.width, win.height, 0.0, -1.0, 10.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glDisable(GL_CULL_FACE);
+	glDisable(GL_DEPTH_TEST);
+        glDisable(GL_LIGHTING);
+        glDisable(GL_LIGHT0);
+	glClear(GL_DEPTH_BUFFER_BIT);
+
+	glBegin(GL_QUADS);
+	    glColor3f(0.8, 0.4, 0.4);
+	    glVertex2f(0.0, 0.0);
+	    glVertex2f(15*win.width/100, 0.0);
+	    glVertex2f(15*win.width/100, win.height);
+	    glVertex2f(0.0, win.height);
+	glEnd();
+	
+	glBegin(GL_QUADS);
+	    glColor3f(0, 0, 0.5);
+	    glVertex2f(10, 400);
+	    glVertex2f(15*win.width/100 - 10, 400);
+	    glVertex2f(15*win.width/100 - 10, win.height-10);
+	    glVertex2f(10, win.height - 10);
+	glEnd();
+	
+	//printing text
+	const char* x = "Pogo Flip\n----------------\nReach targets using\n arrow keys and \ni,j,k,l,a,w,s,d keys \nto score points";
+	unsigned char* y;
+	y = (unsigned char*) x;
+	glColor3f(0,0,0);
+	glRasterPos2i(10, 100);
+	glutBitmapString(GLUT_BITMAP_HELVETICA_18, y);
+
+	// Making sure we can render 3d again
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
 	glutSwapBuffers();	
 	glFlush(); 
+	//cout<<rotx<<" "<<roty<<" "<<rotz<<" "<<p->x<<" "<<p->y<<" "<<p->z<<endl; 
 }
 
 void rotate(int new_angle)
@@ -216,34 +299,82 @@ void Level::keyPress(unsigned char key, int x, int y)
 	{
 		glutTimerFunc(100, rotate , 1 );
 	}
+	else if(key == 'i')
+	{
+		p->curx+=0.3;
+	}
+	else if(key == 'k')
+	{
+		p->curx-=0.3;
+	}
+	else if(key == 'j')
+	{
+		p->curz-=0.3;
+	}
+	else if(key == 'l')
+	{
+		p->curz+=0.3;
+	}
 	else if(key == KEY_ESCAPE)
 	{
 		exit(0);
+	}
+	for(int i=0; i<targets.size(); i++)
+	{
+		if((targets[i].x-p->curx)*(targets[i].x-p->curx)+(targets[i].z-p->curz)*(targets[i].z-p->curz) <= 1 && targets[i].reached == false)
+		{
+			targets[i].reached = true;
+			p->points += targets[i].points;
+		}
 	}
 	glutPostRedisplay();
 }
 
 void Level::specialKeyPress(int key,int x,int y)
 {
-	if ( key == GLUT_KEY_LEFT )
+	if ( key == GLUT_KEY_RIGHT )
 	{
 		roty += 3.0;
 		if(roty>=360)roty-=360;
+		float tx, tz;
+		tx = p->curx;
+		tz = p->curz;
+		p->curx = tx*cos(3.0*PI/180) + (tz + 3.4)*sin(3.0*PI/180);
+		p->curz = (tz + 3.4)*cos(3.0*PI/180) - tx*sin(3.0*PI/180) - 3.4;
 	}
-	if ( key == GLUT_KEY_RIGHT )
+	if ( key == GLUT_KEY_LEFT )
 	{
 		roty -= 3.0;
 		if(roty<=0)roty+=360;
+		float tx, tz;
+		tx = p->curx;
+		tz = p->curz;
+		p->curx = tx*cos((360-3.0)*PI/180)+(tz + 3.4)*sin((360-3.0)*PI/180);
+		p->curz = (tz + 3.4)*cos((360-3.0)*PI/180)-tx*sin((360-3.0)*PI/180) - 3.4;
 	}
 	if ( key == GLUT_KEY_UP )
 	{
-		rotx += 3.0;
-		if(rotx>=360)rotx-=360;
+		p->z+=0.3;
+		p->curz+=0.3*cos(roty*PI/180);
+		p->curx+=0.3*sin(roty*PI/180);
+		//rotx += 3.0;
+		//if(rotx>=360)rotx-=360;
 	}
 	if ( key == GLUT_KEY_DOWN )
 	{
-		rotx -= 3.0;
-		if(rotx<=0)rotx+=360;
+		p->z-=0.3;
+		p->curz-=0.3*cos(roty*PI/180);
+		p->curx-=0.3*sin(roty*PI/180);
+		//rotx -= 3.0;
+		//if(rotx<=0)rotx+=360;
+	}
+	for(int i=0; i<targets.size(); i++)
+	{
+		if((targets[i].x-p->curx)*(targets[i].x-p->curx)+(targets[i].z-p->curz)*(targets[i].z-p->curz) <= 1 && targets[i].reached == false)
+		{
+			targets[i].reached = true;
+			p->points += targets[i].points;
+		}
 	}
 	glutPostRedisplay();
 	//Handle arrow, function keys etc
